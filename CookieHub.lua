d82e59c8067f6a8f037def2997bcc6f7d6fa30b7
Andrewssssssfr <105506928+Andrewssssssfr@users.noreply.github.com>
2022-09-25 19:42:34 +0300

    Update CookieHub.lua

--- CookieHub.lua
+++ CookieHub.lua
@@ -6,4 +6,6 @@ elseif game.PlaceId == 6516141723 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Doors.lua',true))()
 elseif game.PlaceId == 10320240572 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/EasyRaceClicker.lua',true))()
+elseif game.PlaceId == 920587237 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/AdoptMe.lua',true))()
 end
