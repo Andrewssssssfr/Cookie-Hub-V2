3d1beae91b0067d0233a6f5f3a3c21ac25f21247
Andrewssssssfr <105506928+Andrewssssssfr@users.noreply.github.com>
2022-10-03 11:08:04 +0300

    Update CookieHub.lua

--- CookieHub.lua
+++ CookieHub.lua
@@ -4,12 +4,14 @@ elseif game.PlaceId == 9872472334 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Evade.lua',true))()
 elseif game.PlaceId == 6516141723 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Doors.lua',true))()
+elseif game.PlaceId == 6839171747 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Doors.lua',true))()
 elseif game.PlaceId == 10320240572 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/EasyRaceClicker.lua',true))()
 elseif game.PlaceId == 920587237 then
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/AdoptMe.lua',true))()
 elseif game.PlaceId == 3260590327 then
 	   loadstring(game:HttpGet("https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/TDS.lua"))()
-else
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Doors.lua',true))() -- doors 2
+elseif game.PlaceId == 5591597781 then
+	   loadstring(game:HttpGet("https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/TDS.lua"))()
 end
