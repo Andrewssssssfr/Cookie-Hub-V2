59cc3d200396909495e429ec13ece8be1d910c3e
Andrewssssssfr <105506928+Andrewssssssfr@users.noreply.github.com>
2022-10-03 17:21:58 +0300

    Update CookieHub.lua

--- CookieHub.lua
+++ CookieHub.lua
@@ -14,4 +14,8 @@ elseif game.PlaceId == 3260590327 then
 	   loadstring(game:HttpGet("https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/TDS.lua"))()
 elseif game.PlaceId == 5591597781 then
 	   loadstring(game:HttpGet("https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/TDS.lua"))()
+elseif game.PlaceId == 7991339063 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Rainbow%20Friends%F0%9F%8C%88.xml.lua',true))()
+elseif game.PlaceId == 8888615802 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Andrewssssssfr/Cookie-Hub-V2/main/Rainbow%20Friends%F0%9F%8C%88.xml.lua',true))()
 end
